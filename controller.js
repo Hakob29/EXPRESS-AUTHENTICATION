d0d75d90669d1018d6103fae2c5e60514c5ea812
Hakob29 <hakobyan29001@gmail.com>
2023-01-20 21:57:40 +0400

    User created in Db

--- controller.js
+++ controller.js
@@ -1,34 +1,42 @@
 import Role from "./models/Role.js";
 import User from "./models/User.js"
+import bcrypt from "bcrypt";
 
 class Controller {
 
-    register(req, res) {
+    async register(req, res) {
         try {
-
+            const { username, password } = req.body;
+            const candidate = await User.findOne({ where: { username: username } });
+            if (candidate) return res.status(400).json("User with this username exist...");
+            const userRole = await Role.findOne({ value: 'USER' });
+            const user = User.create({
+                username: username,
+                password: await bcrypt.hash(password, 10),
+                roles: [userRole.value]
+            });
+            await user.save;
+            return res.status(200).json("Server created successfully...")
         } catch (err) {
             console.log(err);
         }
     }
-    login(req, res) {
+    async login(req, res) {
         try {
+
 
         } catch (err) {
             console.log(err);
+            res.status(400).json({ message: "Registration error!" })
         }
     }
 
     async getUsers(req, res) {
         try {
-            const userRole = new Role();
-            const adminRole = new Role({ value: "ADMIN" })
 
-            await userRole.save();
-            await adminRole.save();
-
-            res.send("Hello world")
         } catch (err) {
             console.log(err);
+            res.status(400).json({ message: "Login error!" })
         }
     }
 }
